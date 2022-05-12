3898edf3f3d262cb619ce192365ddb5373d1c0ea
Harshuu24 <87519827+Harshuu24@users.noreply.github.com>
2022-05-12 17:41:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,29 +35,6 @@ fruit_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruit_to_show)
 
 
-importrequests
-
-
-my_fruit_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list= my_fruit_list.set_index('Fruit')
-
-
-streamlit.title('My Mom\'s New Healthy Diner')
-streamlit.header('Breakfast Favorites')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
-
-
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
-
-# Let's put a pick list here so they can pick the fruit they want to include
-fruits_selected= streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ["Avocado", "Strawberries"])
-fruit_to_show= my_fruit_list.loc[fruits_selected]
-streamlit.dataframe(fruit_to_show)
-
 
 streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice=streamlit.text_input('What fruit would you like information about?', 'kiwi')
